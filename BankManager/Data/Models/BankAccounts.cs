73a226ab885ee0455f10fc8c5ea56ed336de3664
IMDimov18 <IMDimov18@codingburgas.bg>
2022-06-09 17:34:38 +0300

    Initial Commit

---
+++ BankManager/Data/Models/BankAccounts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankManager.Data.Models
+{
+    public class BankAccounts
+    {
+        public int Id { get; set; }
+        public double Money { get; set; }
+        public int AccNumber { get; set; }
+
+    }
+}
---
+++ BankManager/Data/Models/Banks.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankManager.Data.Models
+{
+    public class Banks
+    {
+        public int Id { get; set; }
+        public string Manager { get; set; }
+        public string Owner { get; set; }
+        public string Support { get; set; }
+    }
+}
